c3a591ae8e0a5a98773d61757f74af62aa1c3aad
jhs <skc05096@naver.com>
2025-04-08 17:37:06 +0900

    cuh수정

--- main/src/main222/main22222.java
+++ main/src/main222/main22222.java
@@ -3,8 +3,8 @@ package main222;
 public class main22222 {
 
 	public static void main(String[] args) {
-		System.out.println("asdasd");
-
+		System.out.println("asdasdddddddddddddd");
+		System.out.println("asdasdddddddddddfffddd");
 	}
 
 }
